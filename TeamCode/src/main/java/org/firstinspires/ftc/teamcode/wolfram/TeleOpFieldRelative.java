c76b14d559da9a14e2c48f3a09c4d697e61680fa
Nathan <11415806+username4723@users.noreply.github.com>
2022-01-13 07:19:51 -0600

    Change speed modifier values to be more logical

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/wolfram/TeleOpFieldRelative.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/wolfram/TeleOpFieldRelative.java
@@ -19,9 +19,9 @@ public class TeleOpFieldRelative extends CustomOpMode {
         super.loop();
 
         // Set the speed modifier
-        if (gamepad1.a) speedModifier = 0.6;
+        if (gamepad1.a) speedModifier = 0.1;
         else if (gamepad1.b) speedModifier = 0.3;
-        else if (gamepad1.x) speedModifier = 0.1;
+        else if (gamepad1.x) speedModifier = 0.6;
         else if (gamepad1.y) speedModifier = 1;
 
         //
